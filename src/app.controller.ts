dba1b7a511451b7216265178e40921c004173fc8
Varga Szabolcs <vargaszabi2001@gmail.com>
2023-01-05 12:14:23 +0100

    teljesen működő állapot

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,10 @@
 import {
+  Body,
   Controller,
   Get,
+  Post,
   Query,
+  Redirect,
   Render,
 } from '@nestjs/common';
 import { AppService } from './app.service';
@@ -20,15 +23,35 @@ export class AppController {
       books: rows,
     };
   }
+
+  @Get('books/new')
+  @Render('form')
+  newBookform() { 
+    return {};
+  }
+
   
   @Get('/keres')
   @Render('list')
-  async cicaSearch(@Query('ertekeles') ertekeles: string)  {
+  async bookSearch(@Query('ertekeles') ertekeles: string)  {
     const [rows] = await db.execute(
       'SELECT title, rating FROM books WHERE rating LIKE ?',[ertekeles],
     );
   
     return { books: rows };
+  }
+
+
+  @Post('books/new')
+  @Redirect()
+  async newBook(@Body() book: BooksDto)  {
+    const [result]: any = await db.execute(
+      'INSERT INTO books (title, rating) VALUES (?, ?)',
+      [book.title, book.rating],
+    );
+    return {
+      url: '/',
+    };
   }
 
 }
